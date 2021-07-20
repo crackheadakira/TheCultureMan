a73528401ef7fe878448bbcd120708815310a117
crackheadakira <33316699+crackheadakira@users.noreply.github.com>
2021-07-20 23:19:46 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const redditFetch = require('reddit-fetch');
+
+client.on('ready', () => {
+	console.log("I'm ready to release");
+});
+
+client.on('message', message => {
+	if(message.content == '>culture'){
+	    redditFetch({
+		
+		     subreddit: 'pics',
+		     sort: 'new',
+		     allowNSFW: true,
+		     allowModPost: false,
+		     allowCrossPost: true,
+			 allowVideo: true,
+		
+	}).then(post => {
+	   message.channel.send(` ${post.url} `);
+	});
+	}
+});
+
+client.login('ODY3MDEwMTMxNzQ1MTc3NjIx.YPa4Gw.H4UPDzYfUCrkQl-doXZoIfZZJ-w');
