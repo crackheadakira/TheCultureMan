d427e9f87718dd4980825c5f4bca4877a49ab0d0
hamed <hamed.hp332@gmail.com>
2021-08-02 01:31:11 +0430

    refactor code

--- commands/culture.js
+++ commands/culture.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
 const Culture69 = require('Culture69').Culture69;
 const c69 = new Culture69();
 
@@ -7,19 +6,22 @@ module.exports = {
     name: "culture",
     description: "This gets you a random image from the database",
     execute(message, args) {
-        if (message.content.startsWith('n.culture')) {
-            if(message.channel.nsfw !== true) {
-                message.reply("get your ass into an NSFW channel");
-            } else {
+        if (!message.content.startsWith('n.culture')) {
+            return;
+        }
+
+        if (!message.channel.nsfw) {
+            message.reply("get your ass into an NSFW channel");
+            return;
+        }
+
 
         let cam69 = c69.getCulture69();
 
         embed = new Discord.MessageEmbed()
-        .setTitle("Here's an image/gif from our Handcrafted database!")
-        .setImage(cam69)
-        
+            .setTitle("Here's an image/gif from our Handcrafted database!")
+            .setImage(cam69)
+
         message.channel.send(embed)
-            }
-        }
     }
 }
