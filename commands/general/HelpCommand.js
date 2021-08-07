88f1580990635f277279b3cb4f06593d9517ad73
crackheadakira <33316699+crackheadakira@users.noreply.github.com>
2021-08-07 16:43:45 +0200

    categorized help command

--- commands/general/HelpCommand.js
+++ commands/general/HelpCommand.js
@@ -1,5 +1,6 @@
 const BaseCommand = require('../../utils/structures/BaseCommand');
 const Discord = require('discord.js');
+const paginationEmbed = require('discord.js-pagination');
 
 module.exports = class HelpCommand extends BaseCommand {
   constructor() {
@@ -7,20 +8,36 @@ module.exports = class HelpCommand extends BaseCommand {
   }
 
   run(client, message, args) {
-    const commandsEmbed = new Discord.MessageEmbed()
+    const cmdEmbed1 = new Discord.MessageEmbed()
       .setColor('eb9840')
-      .setTitle('Here are the fucking commands, beware im an NSFW bot')
+      .setTitle('HENTAI COMMANDS')
       .addFields(
         { name: 'n.', value: "n. is the bot's prefix" },
         { name: 'n.search', value: 'This will search nHentai for the specified Doujin and give you info about it' },
         { name: 'n.culture', value: 'This gets you a random image from the database' },
         { name: 'n.read', value: "This allows you to read a doujin inside of Discord with the homies" },
         { name: 'n.random', value: 'This will give you a random doujin from nHentai' },
-        { name: 'n.anime', value: 'This will search Anilist for the specified Anime and give you info about it' },
-        { name: 'n.manga', value: 'This will search Anilist for the specified Manga and give you info about it' },
-        { name: 'n.activity', value: 'This will fetch the one recent activity of the specified person' },
-        { name: 'n.user', value: 'This will search the specified Anilist user and give you info about them"' },
       )
-    message.channel.send(commandsEmbed)
+
+      const cmdEmbed2 = new Discord.MessageEmbed()
+      .setColor('eb9840')
+      .setTitle('ANILIST COMMANDS')
+      .addFields(
+        { name: 'n.anime', value: 'This will search Anilist for the specified Anime and give you info about it.' },
+        { name: 'n.manga', value: 'This will search Anilist for the specified Manga and give you info about it.' },
+        { name: 'n.activity', value: 'This will fetch the one recent activity of the specified person.' },
+        { name: 'n.user', value: 'This will search the specified Anilist user and give you info about them.' },
+      )
+
+    const pages = [
+      cmdEmbed1,
+      cmdEmbed2
+    ]
+    const emojiList = [
+      "⬅️",
+      "➡️"
+    ];
+
+    paginationEmbed(message, pages, emojiList);
   }
 }
