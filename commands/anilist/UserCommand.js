154ecb7261af44a13516da8b344a591129eb38a9
crackheadakira <33316699+crackheadakira@users.noreply.github.com>
2021-08-17 01:10:41 +0200

    added more info to the user command

--- commands/anilist/UserCommand.js
+++ commands/anilist/UserCommand.js
@@ -18,26 +18,46 @@ module.exports = class UserCommand extends BaseCommand {
     let string = message.content.replace("n.user ", "");
 
 
-    Anilist.user.profile(string).then(data => {
+    Anilist.user.all(string).then(data => {
       try {
 
         let userID = data.id;
         let userName = data.name;
         let userAvatar = data.avatar.large;
         let userURL = data.siteUrl;
+        let userBanner = data.bannerImage;
+
+        // User's Anime Statistics
+        let animeStatistics = data.statistics.anime;
+        let animeMeanScore = animeStatistics.meanScore;
+        let animeWatched = animeStatistics.count;
+        let animeEpisodes = animeStatistics.episodesWatched;
+
+        // User's Manga Statistics
+        let mangaStatistics = data.statistics.manga;
+        let mangaMeanScore = mangaStatistics.meanScore;
+        let mangaRead = mangaStatistics.count;
+        let mangaChapters = mangaStatistics.chaptersRead;
 
         const embed = new Discord.MessageEmbed()
-          .setColor('RANDOM')
-          .setThumbnail(userAvatar)
-          .setTitle(userName)
-          .setURL(userURL)
-          .addFields(
-            { name: 'User ID', value: "This is the user's ID: " + userID },
-          )
+        .setTitle(userName + "'s profile")
+        .setThumbnail(userAvatar)
+        .setImage(userBanner)
+        .setURL(userURL)
+        .addFields(
+          { name: 'User ID', value: userID },
+          { name: "Anime Mean Score", value: animeMeanScore, inline : true },
+          { name: "Anime Watched", value: animeWatched, inline : true },
+          { name: "Episodes Watched", value: animeEpisodes, inline : true },
+          { name: "Manga Mean Score", value: mangaMeanScore, inline : true },
+          { name: "Manga Read", value: mangaRead, inline : true },
+          { name: "Chapters Read", value: mangaChapters, inline : true },
+        )
 
         message.channel.send(embed)
+
       } catch (error) {
-        message.channel.send("Bot received an error. Maybe there was a grammatical mistake?")
+       message.channel.send("Bot received an error. Maybe there was a grammatical mistake?")
       }
     });
   }
