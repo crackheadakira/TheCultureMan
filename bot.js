ad3bafd9583ea6be78c83f79c8ea0053510f2f95
crackheadakira <33316699+crackheadakira@users.noreply.github.com>
2021-08-19 17:18:17 +0200

    updated bot to discord.js v13

--- bot.js
+++ bot.js
@@ -1,15 +1,34 @@
-
-const { Client } = require('discord.js');
-const { registerCommands, registerEvents } = require('./utils/registry');
-const config = require('./config.json');
+console.clear();
+const Client = require("./Structures/client.js");
 const client = new Client();
+const config = require('./config.json');
+const fs = require("fs");
+const Command = require("./Structures/command.js");
 
-(async () => {
-  client.commands = new Map();
-  client.events = new Map();
-  client.prefix = config.prefix;
-  await registerCommands(client, '../commands');
-  await registerEvents(client, '../events');
-  await client.login(config.token);
-})();
+fs.readdirSync("./commands")
+    .filter(file => file.endsWith(".js"))
+    .forEach(file => {
+        /**
+         * @type {Command}
+         */
+        const command = require(`./commands/${file}`);
+        client.commands.set(command.name, command);
 
+    });
+
+client.on("ready", () => console.log("The Culture Man is online."));
+
+client.on("messageCreate", message => {
+
+    if (!message.content.startsWith(config.prefix)) return;
+
+    const args = message.content.substring(config.prefix.length).split(/ +/);
+
+    const command = client.commands.find(cmd => cmd.name == args[0]);
+
+    if(!command) return message.channel.send("There is no command by that name.");
+
+    command.run(message, args, client);
+});
+
+client.login(config.token);
