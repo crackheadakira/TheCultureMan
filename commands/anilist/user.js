c5a28852995df95d9d1c8b2b5c7e4db246fcd127
crackheadakira <lasn.mine@gmail.com>
2022-03-17 16:20:30 +0100

    edited user to be a bit more condensed

--- commands/anilist/user.js
+++ commands/anilist/user.js
@@ -27,90 +27,80 @@ module.exports = {
             try {
 
                 let userID = data.id;
-                let userName = data.name;
+                let userName = data.name.toString();
                 let userAvatar = data.avatar.large;
-                let userURL = data.siteUrl;
+                let userURL = data.siteUrl.toString();
                 let userBanner = data.bannerImage;
                 var vars = {
                     userid: userID,
                 };
 
-                GraphQLRequest(GraphQLQueries.user, vars)
-                    .then((yeezies) => {
+                GraphQLRequest(GraphQLQueries.user, vars).then((yeezies) => {
+
                         let followers = yeezies.Page.pageInfo.total;
 
-                        let donatorTier = data.donatorTier;
+                        let donatorTier = data.donatorTier.toString();
 
                         // User's Anime Statistics
                         let animeStatistics = data.statistics.anime;
-                        let animeMeanScore = animeStatistics.meanScore;
-                        let animeWatched = animeStatistics.count;
-                        let animeEpisodes = animeStatistics.episodesWatched;
+                        let animeMeanScore = animeStatistics.meanScore.toString();
+                        let animeWatched = animeStatistics.count.toString();
+                        let animeEpisodes = animeStatistics.episodesWatched.toString();
 
                         // User's Manga Statistics
                         let mangaStatistics = data.statistics.manga;
-                        let mangaMeanScore = mangaStatistics.meanScore;
-                        let mangaRead = mangaStatistics.count;
-                        let mangaChapters = mangaStatistics.chaptersRead;
-
-                        // User's Favourite Anime & Manga
-                        let animeFavourites = data.favourites.anime;
-                        let mangaFavourites = data.favourites.manga;
+                        let mangaMeanScore = mangaStatistics.meanScore.toString();
+                        let mangaRead = mangaStatistics.count.toString();
+                        let mangaChapters = mangaStatistics.chaptersRead.toString();
 
                         // Sort user's favourite anime
-                        let aTitle = animeFavourites.map(({ title }) => title);
-                        var aTitles = aTitle.map(function (item) {
-                            return item['romaji'];
-                        });
+                        let aTitle = data.favourites.anime.map(({ title }) => title);
+                        let aTitles = aTitle.map(({ romaji }) => romaji);
 
                         // Sort user's favourite manga
-                        let mTitle = mangaFavourites.map(({ title }) => title);
-                        var mTitles = mTitle.map(function (item) {
-                            return item['romaji'];
-                        });
+                        let mTitle = data.favourites.manga.map(({ title }) => title);
+                        let mTitles = mTitle.map(({ romaji }) => romaji);
 
                         // Fixes the titles to be in a good order
-                        aTitles = aTitles.toString();
-                        mTitles = mTitles.toString();
-                        mTitles = mTitles.replace(/,/g, "\n");
-                        aTitles = aTitles.replace(/,/g, "\n");
+                        aTitles = aTitles.toString().replace(/,/g, "\n");
+                        mTitles = mTitles.toString().replace(/,/g, "\n");
 
 
                         const embed = new MessageEmbed()
-                            .setTitle(userName.toString() + "'s profile")
+                            .setTitle(`${userName}'s profile`)
                             .setThumbnail(userAvatar)
                             .setImage(userBanner)
                             .setURL(userURL)
                             .addFields(
                                 { name: "User ID", value: userID.toString(), inline: true },
                                 { name: "Followers", value: followers.toString(), inline: true },
-                                { name: "Donator Tier", value: `Tier ${donatorTier.toString()}`, inline: true },
-                                { name: "Anime Mean Score", value: animeMeanScore.toString(), inline: true },
-                                { name: "Anime Watched", value: animeWatched.toString(), inline: true },
-                                { name: "Episodes Watched", value: animeEpisodes.toString(), inline: true },
-                                { name: "Manga Mean Score", value: mangaMeanScore.toString(), inline: true },
-                                { name: "Manga Read", value: mangaRead.toString(), inline: true },
-                                { name: "Chapters Read", value: mangaChapters.toString(), inline: true },
+                                { name: "Donator Tier", value: `Tier ${donatorTier}`, inline: true },
+                                { name: "Anime Mean Score", value: animeMeanScore, inline: true },
+                                { name: "Anime Watched", value: animeWatched, inline: true },
+                                { name: "Episodes Watched", value: animeEpisodes, inline: true },
+                                { name: "Manga Mean Score", value: mangaMeanScore, inline: true },
+                                { name: "Manga Read", value: mangaRead, inline: true },
+                                { name: "Chapters Read", value: mangaChapters, inline: true },
                             )
                             .setFooter("Requested by " + message.author.username)
 
                         const favouritesEmbed = new MessageEmbed()
-                            .setTitle(userName + "'s favourites")
+                            .setTitle(`${userName}'s favourites`)
                             .setThumbnail(userAvatar)
                             .setURL(userURL)
                             .addFields(
                                 { name: "Favourite Anime", value: `${aTitles || "No Favourites"}` },
                                 { name: "Favourite Manga", value: `${mTitles || "No Favourites"}` },
                             )
-                            .setFooter("Requested by " + message.author.username)
+                            .setFooter(`Requested by ${message.author.username}`)
 
                         const pages = [embed, favouritesEmbed]
                         paginationEmbed(paginationOpts(message, pages))
 
                     });
             } catch (error) {
-                message.channel.send("The user doesn't exist or you haven't set your AniList user, set it using n.setuser.")
-                return console.log(error);
+                message.channel.send("``" + error + "``");
+                console.log(error);
             }
         });
     }
